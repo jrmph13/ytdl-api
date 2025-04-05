a90739fbf5f056b8980b8068676f20a35bfe7849
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 17:11:40 +0000

    chore(deps): restructure view templates

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd054b7e29b446fda2c2f06f2fa494a3 = () => {
-  return '2025-04-01T20:40:53Z';
+const node_872620014db34decafe51a00b78ae352 = () => {
+  return '2025-04-05T17:11:40Z';
 };
