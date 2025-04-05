b980a6aed55deb26ed238c445610dd24828d93bf
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 08:37:38 +0000

    refactor(worker): restructure payload parser

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49996be55c7341d79b5a89b36d058850 = () => {
-  return '2025-03-28T23:42:01Z';
+const node_cff5744a49ce4371a306d061d221049b = () => {
+  return '2025-04-05T08:37:38Z';
 };
