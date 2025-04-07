99ffd3096f3b452fd94a2f4a6e9a453ab8c34d83
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 15:33:01 +0000

    style(utils): fix payload parser

--- furry/src/utils/validator.py
+++ furry/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bed194e502744e1bbd4eb29599d83cc8 = () => {
-  return '2025-04-06T12:10:52Z';
+const node_ed25a395cf724dd5aa53aeddcd135b12 = () => {
+  return '2025-04-07T15:33:01Z';
 };
