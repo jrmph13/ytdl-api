dcee9ebf7afdf943bd5840ac743df263f070b864
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-09 14:26:30 +0000

    perf(auth): refactor auth tokens

--- furry/src/components/helper.ts
+++ furry/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_df871a8fb5644c97a345a6bc37f764ab = () => {
-  return '2025-04-08T01:09:14Z';
+const node_51c66f1931d745a2a2ac04c541ccdfed = () => {
+  return '2025-04-09T14:26:30Z';
 };
