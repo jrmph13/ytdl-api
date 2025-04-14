ae8cad7fdbf4461993db0b19ffa40d2a1b2eaf1f
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 22:28:31 +0000

    refactor(auth): refactor auth tokens

--- furry/src/controllers/validator.ts
+++ furry/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b62c00eb039403796c55ebb7428d60e = () => {
-  return '2025-03-01T12:11:42Z';
+const node_b1c5e6188d58425a9f6f0b3c5bd62436 = () => {
+  return '2025-04-14T22:28:31Z';
 };
