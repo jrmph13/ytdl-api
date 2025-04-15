44a1f3314cd344306c71e1b1eb175ac1c18314d1
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-15 15:23:39 +0000

    fix(config): fix validation pipeline

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8483adb2fd364e5f92af9fb059a47b52 = () => {
-  return '2025-03-29T09:36:26Z';
+const node_5a38a04b97ac44d5943c1c9214f089dd = () => {
+  return '2025-04-15T15:23:39Z';
 };
