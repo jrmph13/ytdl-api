cb6597eeca5dc8b37451bc2a5c770c8a6c95df18
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 20:17:48 +0000

    chore(auth): integrate auth tokens

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b23504eccea54a42937ed6795fa9e754 = () => {
-  return '2025-04-16T02:54:17Z';
+const node_f040041134fc4e16984f22ae2829211a = () => {
+  return '2025-04-17T20:17:48Z';
 };
