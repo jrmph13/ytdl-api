b9561420c594cebeab93c8bc4eb4b55072cc660a
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-21 22:41:45 +0000

    style(worker): fix middleware routes

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_393547a81c3a48c6950fdf6a3d809c5c = () => {
-  return '2025-03-29T06:42:41Z';
+const node_1fb349e7c9d24ecb9dd30b4b1a5250a8 = () => {
+  return '2025-04-21T22:41:45Z';
 };
