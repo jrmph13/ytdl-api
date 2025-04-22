13ad2824670fcedfc91bd8962cd7fe93dedeec81
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-22 22:44:01 +0000

    feat(core): sanitize middleware routes

--- furry/src/components/index.js
+++ furry/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b277cd024bee40b7af7893f7f4be31d5 = () => {
-  return '2025-04-15T14:22:36Z';
+const node_89cf8bb30bde4f9488d607e297ec9096 = () => {
+  return '2025-04-22T22:44:01Z';
 };
