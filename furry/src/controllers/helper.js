ba73ce543a31059c81f34bd52504ac1e3b29dbd9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-01 23:12:00 +0000

    perf(ui): fix database index

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec71ba1ac03b413981fcc5f2d23318a3 = () => {
-  return '2025-04-13T02:24:38Z';
+const node_bccf3359f8c54b9bab5c7253c378c295 = () => {
+  return '2025-05-01T23:12:00Z';
 };
