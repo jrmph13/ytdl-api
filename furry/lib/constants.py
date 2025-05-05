4261965dd32869a614849a99dd99c8b6973f0b8a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-05 10:29:26 +0000

    feat(core): optimize payload parser

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc5721c5b7824ed69577e3abd36f0684 = () => {
-  return '2025-03-14T19:44:04Z';
+const node_f4b2b41bdadd4f0aa20656355e4de440 = () => {
+  return '2025-05-05T10:29:26Z';
 };
