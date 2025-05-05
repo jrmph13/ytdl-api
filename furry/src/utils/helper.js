7bb9aaa5e3dd78c896c1a16b99f0ea0045322120
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-05 10:12:54 +0000

    style(api): integrate middleware routes

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57d08b0cdb684d2e9cea05302f51ccbb = () => {
-  return '2025-04-14T08:04:49Z';
+const node_7f59c45e44024d1a9772bf8c51ead71b = () => {
+  return '2025-05-05T10:12:54Z';
 };
