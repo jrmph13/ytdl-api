0c2de1dde7c5c07debc5e3e2cc0114b8196d3ac5
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-06 22:08:29 +0000

    fix(utils): update database index

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_df45bb09ffd24abab871b51e0c0cb729 = () => {
-  return '2025-04-07T08:14:41Z';
+const node_0a7acee324e34a4b988e299b0c585f66 = () => {
+  return '2025-05-06T22:08:29Z';
 };
