82ba9a607f8fedddfbed382c3e936fa973a64d84
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 05:56:17 +0000

    fix(ui): decouple database index

--- furry/config/index.py
+++ furry/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_137473898e224f83ae15233b5c487307 = () => {
-  return '2025-03-28T05:32:07Z';
+const node_0d57b8fb201b4cd193f531b77658cc62 = () => {
+  return '2025-05-07T05:56:17Z';
 };
