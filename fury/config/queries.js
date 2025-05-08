d60d51356a2504c60745ac3917723781ed45eb67
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 15:21:34 +0000

    chore(utils): integrate view templates

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7278d23723f495581dd9fea5691287c = () => {
-  return '2025-04-29T15:01:42Z';
+const node_50f8aa17d1a74a138f0efa98f21d3b82 = () => {
+  return '2025-05-08T15:21:34Z';
 };
