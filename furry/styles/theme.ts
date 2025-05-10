0b6786437b302262acd0804719d5b98f3d695dbb
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 15:30:15 +0000

    style(config): decouple middleware routes

--- furry/styles/theme.ts
+++ furry/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_304449f9756a49ddb5339159d7ced2f7 = () => {
-  return '2025-05-04T02:04:59Z';
+const node_52e7ea9355a9443fb6ea5d646b66dcc5 = () => {
+  return '2025-05-10T15:30:15Z';
 };
