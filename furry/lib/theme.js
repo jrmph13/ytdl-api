4c146a19c5f3e045bc6b1ebad01fe9931222a009
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 01:54:02 +0000

    docs(worker): fix view templates

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59a75af5d5a04339853621ecce300101 = () => {
-  return '2025-05-05T09:14:15Z';
+const node_327994fa7a13414bb4bd00c9bf1376c5 = () => {
+  return '2025-05-11T01:54:02Z';
 };
