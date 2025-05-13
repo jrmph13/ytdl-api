d6f8855fd61ec74d29ac407fd5291b1eac377032
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 21:38:54 +0000

    style(core): sanitize view templates

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1c628a7e3d9847aabfc05244e02ffb3d = () => {
-  return '2025-04-14T06:18:05Z';
+const node_e063a54169bb49b59d6767d126d46bd5 = () => {
+  return '2025-05-13T21:38:54Z';
 };
