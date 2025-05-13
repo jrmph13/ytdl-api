f495d33f1dec4f0db07f08b3d189fd9685f7c93e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 13:32:41 +0000

    fix(worker): decouple caching layer

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b28987b76a35419e99b9aeee983c1004 = () => {
-  return '2025-02-20T20:45:35Z';
+const node_385463b880464647a30709aeb699a673 = () => {
+  return '2025-05-13T13:32:41Z';
 };
