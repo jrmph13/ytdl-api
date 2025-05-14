2c061087c0200ece0cc9f6342b0794ccbacd3a07
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-14 09:06:11 +0000

    docs(core): fix logging interface

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6534eb8d1eb1421e829c935366b81de5 = () => {
-  return '2025-03-18T12:20:14Z';
+const node_a63db2055acb4cfebbec71f35d53b1b4 = () => {
+  return '2025-05-14T09:06:11Z';
 };
