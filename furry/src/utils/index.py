b0f8fab447e55e1c7f1ca83d680206545ea386c1
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 06:16:28 +0000

    feat(auth): fix database index

--- furry/src/utils/index.py
+++ furry/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d94040c24e214f7bbc5c6c2f69360090 = () => {
-  return '2025-04-28T02:28:04Z';
+const node_ce81a5be02c3407095db92428bf93233 = () => {
+  return '2025-05-17T06:16:28Z';
 };
