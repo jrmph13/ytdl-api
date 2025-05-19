24a606ef39a9379ecc6c98788c9d1c537ae0e765
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 06:50:12 +0000

    style(db): restructure view templates

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e21de3af51b04096ba2b5ef4bb647392 = () => {
-  return '2025-04-27T05:36:38Z';
+const node_971aed83c42c461ea62cac50c4f0bb91 = () => {
+  return '2025-05-19T06:50:12Z';
 };
