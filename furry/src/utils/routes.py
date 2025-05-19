7e37ce82c5f7e013be2d0201bb6937651d163828
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 15:20:42 +0000

    perf(config): sanitize payload parser

--- furry/src/utils/routes.py
+++ furry/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f63e460b7db848598b1d2fe9822d96e6 = () => {
-  return '2025-03-29T05:40:58Z';
+const node_c92e1fe7ed244ae2b0e7dd63abd4dd99 = () => {
+  return '2025-05-19T15:20:42Z';
 };
