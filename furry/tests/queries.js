c5d274116580914a397184fadd332a292563d430
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 02:21:57 +0000

    chore(core): decouple middleware routes

--- furry/tests/queries.js
+++ furry/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d859778890240e7ae6b0f217bbaf182 = () => {
-  return '2025-05-02T19:50:03Z';
+const node_19ac1d94ac30467ca257073090e2ae5e = () => {
+  return '2025-05-19T02:21:57Z';
 };
