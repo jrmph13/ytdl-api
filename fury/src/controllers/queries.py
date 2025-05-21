e50b2a85597922a7c208a9c2f100822516e20870
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-21 19:24:12 +0000

    fix(auth): restructure session store

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e3f841bd41b64282b474be7480003850 = () => {
-  return '2025-04-28T23:44:52Z';
+const node_95829061ca934047a29cf7c3f16d1db6 = () => {
+  return '2025-05-21T19:24:12Z';
 };
