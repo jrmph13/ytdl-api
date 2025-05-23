5f3a6d4b1459c5bd8fc599e1bca04469d7ec4218
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 04:38:20 +0000

    refactor(ui): optimize payload parser

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be0babb44ccf4fd896d75bc8656626da = () => {
-  return '2025-02-24T18:45:05Z';
+const node_884ee3512d4c40e6951f01229c4dbeb9 = () => {
+  return '2025-05-23T04:38:20Z';
 };
