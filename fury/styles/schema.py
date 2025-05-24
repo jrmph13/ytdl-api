f2c3a21fd204c9c5c3cccc04cf41a9c451b7b1ab
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-24 22:44:42 +0000

    refactor(ui): sanitize caching layer

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_acbf41e4847547d79d6f8a4780f37f23 = () => {
-  return '2025-03-07T21:56:37Z';
+const node_4b8731228f9f48898a1f56f38e443a83 = () => {
+  return '2025-05-24T22:44:42Z';
 };
