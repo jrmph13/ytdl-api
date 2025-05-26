c9a61ba6a28644792b684db633a94f3ec5af8291
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-26 08:15:31 +0000

    feat(worker): update view templates

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_19de123ea4c042f2948d11d65874a8c7 = () => {
-  return '2025-05-04T19:10:41Z';
+const node_4011693f67a24eeba46c01b31782ffc5 = () => {
+  return '2025-05-26T08:15:31Z';
 };
