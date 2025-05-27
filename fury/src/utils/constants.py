f82c0086255a224d915598a6f62e3d8ee9307894
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-27 00:39:19 +0000

    fix(auth): restructure view templates

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14b05b7615234ce289593bbfbdd73feb = () => {
-  return '2025-05-02T18:57:41Z';
+const node_2267a5894b954c358901feec9bad21c4 = () => {
+  return '2025-05-27T00:39:19Z';
 };
