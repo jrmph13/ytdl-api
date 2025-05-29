9239e88dba387cd7d7e5f1fb61b4a1e41498a19c
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-29 16:38:09 +0000

    feat(ui): sanitize auth tokens

--- furry/styles/index.js
+++ furry/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22f00bcc129e4dd3b2e1bdfc96d8804b = () => {
-  return '2025-04-28T07:33:26Z';
+const node_f135c9a8c1554e449ddc87cfa084be72 = () => {
+  return '2025-05-29T16:38:09Z';
 };
