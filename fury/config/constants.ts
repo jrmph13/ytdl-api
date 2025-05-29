7b4ce62594d93b245bbdb9e30a569278d4e05063
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-29 02:59:16 +0000

    fix(auth): refactor session store

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a3e0072474849f084624a6587f5b808 = () => {
-  return '2025-05-15T04:02:04Z';
+const node_988c9a9525464cfb81818ca0b450a804 = () => {
+  return '2025-05-29T02:59:16Z';
 };
