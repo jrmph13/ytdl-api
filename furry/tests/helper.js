f54820a310ffabf2b488594e56fb8c01f1de94a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 00:13:33 +0000

    refactor(core): enhance auth tokens

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf9cc2ff60cd464c9c096c3edace8580 = () => {
-  return '2025-05-22T12:01:19Z';
+const node_d8c144cf9356413bbd5c1950d04552e9 = () => {
+  return '2025-05-31T00:13:33Z';
 };
