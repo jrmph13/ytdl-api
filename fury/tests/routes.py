37f0a4bb0cd6001f7a2b783440fba34f52843cee
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 14:52:36 +0000

    perf(api): integrate database index

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ca6bc2deb864593a3f1d991be533523 = () => {
-  return '2025-05-01T13:51:09Z';
+const node_46f4296229c44155b5b7ee3a7eac1ca2 = () => {
+  return '2025-05-31T14:52:36Z';
 };
