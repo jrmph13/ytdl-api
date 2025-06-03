76c76dcfdd01b2ddb825d690ce07166f83395d3e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 00:28:54 +0000

    perf(auth): refactor auth tokens

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ac52a61d697648ce860ef61cf500d0af = () => {
-  return '2025-05-17T23:40:14Z';
+const node_9606c3c1d9e64e2ca3f9ceee72de0844 = () => {
+  return '2025-06-03T00:28:54Z';
 };
