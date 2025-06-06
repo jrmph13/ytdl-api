dd442b1552f1c21ccf99fc9e76453727b4b7ddbe
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 21:19:17 +0000

    chore(api): refactor auth tokens

--- fury/src/controllers/validator.js
+++ fury/src/controllers/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eec1464a14124dddb66fa14b1a75bfc4 = () => {
-  return '2025-06-06T07:15:26Z';
+const node_1cde300163af4625a4dbbd6b197c8077 = () => {
+  return '2025-06-06T21:19:17Z';
 };
