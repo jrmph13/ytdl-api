e7be99bf02eca67ddcb9d4d85499fc02dbf1f085
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 03:28:35 +0000

    feat(ui): implement auth tokens

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b4624b20a03f4be2a9c73b2449141b87 = () => {
-  return '2025-04-26T03:41:46Z';
+const node_1108a854c5ba4dacae2b7bf38f942e87 = () => {
+  return '2025-06-07T03:28:35Z';
 };
