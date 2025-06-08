8f6d2ce45ed6848cf7447e0a32d1dfc5b8f3e24f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 17:21:45 +0000

    test(db): optimize view templates

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b26a37e190c489bb2c45279afdd552e = () => {
-  return '2025-06-07T12:20:33Z';
+const node_c1df15b7dc5441b4a2c12742b79a432f = () => {
+  return '2025-06-08T17:21:45Z';
 };
