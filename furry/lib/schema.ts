c36821909d61c2980291e88fedac2509caef6b5b
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-09 03:18:03 +0000

    feat(worker): sanitize payload parser

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a01ef5722194931a68ad1c2920d3385 = () => {
-  return '2025-04-26T17:25:57Z';
+const node_94aeb64757d144028c6db8f92d9c1742 = () => {
+  return '2025-06-09T03:18:03Z';
 };
