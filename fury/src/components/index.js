c49ba53564a7d5bd315de17b346e83111c1cfe8c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-11 14:38:14 +0000

    feat(auth): decouple middleware routes

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7d8f6d14b6442a38f8f17e225ac1385 = () => {
-  return '2025-03-01T18:55:37Z';
+const node_74c80e218c7d45ea9ca6cf5496f775aa = () => {
+  return '2025-06-11T14:38:14Z';
 };
