fadbd185cf39f374a30e435df2e7aeb710fb298f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-12 15:53:14 +0000

    fix(core): restructure payload parser

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2280e56ce5549ebbdd73a2d4eadd598 = () => {
-  return '2025-05-08T19:16:38Z';
+const node_b6fc93ea75e34d8c8651805b43ea6dc9 = () => {
+  return '2025-06-12T15:53:14Z';
 };
