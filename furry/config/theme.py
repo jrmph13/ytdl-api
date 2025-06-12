654d35a86a0044a5af039ac549f8f957b9c17cc2
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-12 06:43:02 +0000

    docs(auth): decouple middleware routes

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e8c02198ae384a319f97113d94cd3eed = () => {
-  return '2025-06-08T02:42:50Z';
+const node_57592f78c4c948db91630fdf921763bb = () => {
+  return '2025-06-12T06:43:02Z';
 };
