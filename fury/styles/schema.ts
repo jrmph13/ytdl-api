63d6d29d4b84323b5fab04012b6f08b4983b0074
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-17 21:41:19 +0000

    feat(auth): sanitize auth tokens

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad8e53f96e1a4330899dc1cb49efeaf1 = () => {
-  return '2025-04-29T21:42:36Z';
+const node_0f7bb6971d5f423fb01fa039510345a1 = () => {
+  return '2025-06-17T21:41:19Z';
 };
