6536da4e6141b9d428f9436b29706b84c0e98a11
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 04:50:51 +0000

    fix(utils): optimize caching layer

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7403ea8fcbad47ce978172a5b4f5b2e3 = () => {
-  return '2025-06-02T01:00:13Z';
+const node_f0e5122ef18143dea95993bd50393886 = () => {
+  return '2025-06-21T04:50:51Z';
 };
