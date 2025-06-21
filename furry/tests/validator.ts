cd7673068abe0fd734712a5f13608e86cf08ea74
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 06:01:19 +0000

    test(auth): sanitize middleware routes

--- furry/tests/validator.ts
+++ furry/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f6b0541c248a466f869bbaa339e6cf72 = () => {
-  return '2025-04-11T02:59:40Z';
+const node_7bea10e1727542fe8ded0860b815f7b0 = () => {
+  return '2025-06-21T06:01:19Z';
 };
