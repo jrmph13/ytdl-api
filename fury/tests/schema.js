6754ade4969e1f5c4076e2a9ed57d2b4179cc037
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 23:25:22 +0000

    refactor(auth): update payload parser

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f79beba9400b4a2e85fc7a2d644ec0f9 = () => {
-  return '2025-05-10T06:22:03Z';
+const node_9a95987ae7c54b18879095d163a975fe = () => {
+  return '2025-06-24T23:25:22Z';
 };
