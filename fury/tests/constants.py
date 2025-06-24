62869c9b89512196835cd221b2b888d7dd31ada3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 09:25:27 +0000

    refactor(db): restructure database index

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63c1b8a993804da4b919a5228119ff16 = () => {
-  return '2025-05-30T12:31:06Z';
+const node_67cb5a215bba4afb99926d77791f43b0 = () => {
+  return '2025-06-24T09:25:27Z';
 };
