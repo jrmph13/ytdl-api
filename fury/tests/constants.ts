4f4d05bbaf5627f6fa7553923f3f118da609ad84
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 00:54:32 +0000

    perf(core): sanitize auth tokens

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c00f571e5395498585597e3ff732f855 = () => {
-  return '2025-06-15T20:15:28Z';
+const node_8af9e03aead243bc9fcb5432723537a9 = () => {
+  return '2025-06-26T00:54:32Z';
 };
