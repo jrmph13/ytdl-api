92b24a0ce372eae8a4271289c6ae61c1fe930d26
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 21:52:01 +0000

    chore(core): fix session store

--- fury/src/controllers/validator.ts
+++ fury/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f61c6ce76404130a35cbeea6987d7d4 = () => {
-  return '2025-06-19T17:28:26Z';
+const node_b254a4277ed24a2eb299cf11f7b30488 = () => {
+  return '2025-06-27T21:52:01Z';
 };
