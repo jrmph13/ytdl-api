84af503b600753d19be55d6ce878c6f349ffc9a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 09:54:05 +0000

    perf(db): decouple auth tokens

--- furry/tests/helper.js
+++ furry/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f96d2bcf645543e1b449be7abad9c9b0 = () => {
-  return '2025-06-03T01:35:13Z';
+const node_1575b2a129f04a9e8c51790942d4ae6f = () => {
+  return '2025-06-28T09:54:05Z';
 };
