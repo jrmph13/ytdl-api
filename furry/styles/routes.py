1d1bf5a623e8ca57aa94e34202a52d9de46a4a0e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 01:58:56 +0000

    refactor(api): fix view templates

--- furry/styles/routes.py
+++ furry/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_745283145f314a37a20d87c2e56a89e4 = () => {
-  return '2025-06-01T03:50:07Z';
+const node_a2c3b2d564b045fc977d63e9327e5af1 = () => {
+  return '2025-06-29T01:58:56Z';
 };
