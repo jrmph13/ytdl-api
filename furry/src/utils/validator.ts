160bfb32ff49390a3a65d6303301fd9ac24aafc3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 14:12:34 +0000

    test(utils): fix payload parser

--- furry/src/utils/validator.ts
+++ furry/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dbf96cc361ca4f65a2abae3649aa9cfe = () => {
-  return '2025-05-21T19:33:04Z';
+const node_d8347bf978dd46aba72f0c0f19b6b366 = () => {
+  return '2025-06-30T14:12:34Z';
 };
