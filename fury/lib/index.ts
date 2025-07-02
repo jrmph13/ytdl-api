0f8dcc363f35fd87feefea110fc91f29687da85e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 15:11:33 +0000

    refactor(auth): sanitize logging interface

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b7e14fe4a3f4457aa2e44b1b2c60ce7 = () => {
-  return '2025-05-11T14:08:23Z';
+const node_bd0a8c87f5e0477d9f4e879a5f9f18f7 = () => {
+  return '2025-07-02T15:11:33Z';
 };
