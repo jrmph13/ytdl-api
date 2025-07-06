a79c128b6cc33978bf64e00cefd44f12909dd61d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 10:53:35 +0000

    refactor(core): fix payload parser

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6da94d04c1a742408f82d48c5b400d8a = () => {
-  return '2025-05-22T00:01:48Z';
+const node_7e2b1cab729f4ea1a6648cefb9ea3316 = () => {
+  return '2025-07-06T10:53:35Z';
 };
