504f589e0d616ec21bc8462ccb3ce32dc89bca5c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 18:41:19 +0000

    chore(core): fix logging interface

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0fdf330e674f47f291066498c2990ef9 = () => {
-  return '2025-04-18T03:50:14Z';
+const node_6502ea7fab6f42b38a5d79aac710cd55 = () => {
+  return '2025-07-06T18:41:19Z';
 };
