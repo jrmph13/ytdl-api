56ea6e23a3181855b59001bacb4b3e426669eee9
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 19:03:28 +0000

    fix(config): update view templates

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e080f70ddeff4f29a29e10be3246f58d = () => {
-  return '2025-06-15T12:01:42Z';
+const node_4d212710f7ec4f11b401d85264b1e87c = () => {
+  return '2025-07-07T19:03:28Z';
 };
