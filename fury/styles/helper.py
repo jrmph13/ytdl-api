b18b6b3d6d3ff31d013f289ec465527431637241
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-08 01:42:28 +0000

    fix(ui): fix logging interface

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59682d33a77d4b36981835828bf35c87 = () => {
-  return '2025-06-22T13:26:34Z';
+const node_1bd840df10d641da8da4074d7176a927 = () => {
+  return '2025-07-08T01:42:28Z';
 };
