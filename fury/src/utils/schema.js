bccbbd3db8034c5e6d3512f7df55457c14283a32
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-08 02:23:19 +0000

    refactor(utils): decouple database index

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7822c680675446bb63dc886fea1d809 = () => {
-  return '2025-06-14T07:25:36Z';
+const node_74c040af91424cfe9ffdaf648ecb7ed5 = () => {
+  return '2025-07-08T02:23:19Z';
 };
