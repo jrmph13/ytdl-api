cbedc1c001b6be6120575b5b629a22198305548e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 11:22:22 +0000

    fix(auth): optimize logging interface

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fa472f54ae934346b06d15a09e5f02ed = () => {
-  return '2025-07-05T08:45:40Z';
+const node_efd32cb5597e4d1b8d7802e0af540624 = () => {
+  return '2025-07-17T11:22:22Z';
 };
