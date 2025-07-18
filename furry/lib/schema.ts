7fb222fd5baca4102bea7fa9d3c51d7b44b249e3
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 00:58:20 +0000

    chore(auth): sanitize session store

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d212710f7ec4f11b401d85264b1e87c = () => {
-  return '2025-07-07T19:03:28Z';
+const node_98ab931eba574973a7e3772e2e834e16 = () => {
+  return '2025-07-18T00:58:20Z';
 };
