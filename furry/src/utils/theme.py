e4a5f09bf993fa9e92f06176433bffbbf4bb24b0
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 12:26:43 +0000

    fix(config): restructure view templates

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bb8e6d79c1cb49d0805dae603151b422 = () => {
-  return '2025-05-01T05:08:09Z';
+const node_a2488d35fd2e4487894568909e5b0031 = () => {
+  return '2025-07-19T12:26:43Z';
 };
