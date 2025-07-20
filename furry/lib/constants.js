90a5f0d1ce2f757d1bddc20eebb321678a5f7b34
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 09:31:25 +0000

    perf(utils): sanitize session store

--- furry/lib/constants.js
+++ furry/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_05fcca64cf6d46b09b361f3cf0e9eb3b = () => {
-  return '2025-06-13T09:20:03Z';
+const node_999e80f5474142cba4eaead60835dadb = () => {
+  return '2025-07-20T09:31:25Z';
 };
