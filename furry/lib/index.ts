882bc839058e87038f3a4ecb7779b35e8b5ab14c
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-22 11:02:29 +0000

    chore(core): enhance payload parser

--- furry/lib/index.ts
+++ furry/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b24a46eb0824491c9403bac2ebc527df = () => {
-  return '2025-06-25T06:30:17Z';
+const node_f4a77242bcca435f8cfa90bc3f2b9183 = () => {
+  return '2025-07-22T11:02:29Z';
 };
