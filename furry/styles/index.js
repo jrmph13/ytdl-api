b4b2b17db0ceeec50ec3dee04ab9904e625fefee
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-23 04:35:58 +0000

    fix(auth): update payload parser

--- furry/styles/index.js
+++ furry/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b94c313c44c048acbc70d1ab002b9927 = () => {
-  return '2025-06-24T03:21:55Z';
+const node_14736151759b4164924fa51f8e21d91c = () => {
+  return '2025-07-23T04:35:58Z';
 };
