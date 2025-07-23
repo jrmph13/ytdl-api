81115491936894fb3f876d2f7c852d64afc6c556
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-23 21:00:42 +0000

    fix(auth): update session store

--- furry/styles/theme.ts
+++ furry/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_07a7f8ca2d06433590ba38b7e38c4f21 = () => {
-  return '2025-05-23T00:08:12Z';
+const node_acfdd00b99754d05b691ffbacdfe08e5 = () => {
+  return '2025-07-23T21:00:42Z';
 };
