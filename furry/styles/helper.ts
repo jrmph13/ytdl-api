0a5d94bbb39007f63d96f465b79385344e7d66bc
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 06:06:29 +0000

    docs(auth): update payload parser

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d5f90903f3b7423c8bd795214e19dfe7 = () => {
-  return '2025-05-21T07:45:27Z';
+const node_ff87825c79674bdbaf328e31b2a1eea6 = () => {
+  return '2025-07-25T06:06:29Z';
 };
