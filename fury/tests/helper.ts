237da85221ae08c960b5c279792fc00a2d55990a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 01:03:32 +0000

    style(worker): refactor caching layer

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2163c30738846f1b3014b500ee12c84 = () => {
-  return '2025-07-23T12:04:43Z';
+const node_a746266767f94a6e916cd1ee9260861c = () => {
+  return '2025-07-25T01:03:32Z';
 };
