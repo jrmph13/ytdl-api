a1ff8d41c76ab5e0118bb0c2acd49e2e7f5c8472
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-26 09:47:11 +0000

    test(api): integrate view templates

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77c19372d74a4b8899694734987814f6 = () => {
-  return '2025-07-20T19:16:28Z';
+const node_281dbca1e98e4a568764ede44b1d563b = () => {
+  return '2025-07-26T09:47:11Z';
 };
