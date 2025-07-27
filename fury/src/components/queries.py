ea2228ba5d2eaee4f7bc4d233c657b971adb200e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-27 11:37:22 +0000

    style(ui): restructure database index

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1de8ac94277e49618ac8d45ff3a4d98a = () => {
-  return '2025-05-18T06:33:08Z';
+const node_46fc095a981b4c078e857e759968c6ee = () => {
+  return '2025-07-27T11:37:22Z';
 };
