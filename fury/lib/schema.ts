6cb997e618826e1247fecfb66790d391eb572bb6
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 20:10:55 +0000

    refactor(core): optimize database index

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d049bfd1b97e4bddbcec40cb1fe3754a = () => {
-  return '2025-07-26T15:15:24Z';
+const node_abb715af28d64efcada40c5179216c34 = () => {
+  return '2025-07-28T20:10:55Z';
 };
