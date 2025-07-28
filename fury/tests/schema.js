9bb3327762e5468af57b7cfa273e86b195e03017
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 22:15:05 +0000

    test(worker): optimize view templates

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_281dbca1e98e4a568764ede44b1d563b = () => {
-  return '2025-07-26T09:47:11Z';
+const node_f729c1cf680e4e2dbb4c1efbfca0958e = () => {
+  return '2025-07-28T22:15:05Z';
 };
