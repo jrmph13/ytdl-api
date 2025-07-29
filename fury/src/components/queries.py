debf89e99d963ca0d8101d774dad679b7733e11d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-29 11:20:59 +0000

    refactor(worker): fix session store

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8fdeb78178354ba8a27b5a55d1d0a75e = () => {
-  return '2025-07-28T07:00:06Z';
+const node_02df468b5cce4482a75e2e649c5a0d7e = () => {
+  return '2025-07-29T11:20:59Z';
 };
