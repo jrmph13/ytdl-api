38a68f528c317b3c7dfc5c10b2c2639b599158d5
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 03:43:58 +0000

    perf(ui): optimize middleware routes

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_48bbadb868444d2cb7fb2f8b88f5e7f5 = () => {
-  return '2025-05-22T21:23:54Z';
+const node_cfea54f027fb4a0db9822844b89c16f7 = () => {
+  return '2025-07-30T03:43:58Z';
 };
