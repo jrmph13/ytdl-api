cc611545598408095c4a365ea9e2b061915f623f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 16:13:33 +0000

    chore(core): sanitize view templates

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cef80e41d8f44591b9c9f5fe9aea9cc8 = () => {
-  return '2025-07-15T12:04:29Z';
+const node_4fa26c334fd64294be17038672d0dc7a = () => {
+  return '2025-07-30T16:13:33Z';
 };
