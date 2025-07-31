12d702afe317849b40a8b1f0f14c1cc0c2aced70
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 19:16:33 +0000

    perf(db): fix view templates

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8e7ee6321e4c40e59074da5169d6521a = () => {
-  return '2025-07-10T19:39:12Z';
+const node_e9997af2d9ad4f148e07dcd5a85f2349 = () => {
+  return '2025-07-31T19:16:33Z';
 };
