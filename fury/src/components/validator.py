b59440366023e9cc6985502333f3ff3814ce9302
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 05:58:29 +0000

    fix(config): sanitize payload parser

--- fury/src/components/validator.py
+++ fury/src/components/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed9d2846c5a74c14abcea0dd237eec0d = () => {
-  return '2025-07-19T00:25:20Z';
+const node_dbff74f9c7494d1ab3adef106bc204e5 = () => {
+  return '2025-08-02T05:58:29Z';
 };
