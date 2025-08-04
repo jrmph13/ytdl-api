586d158d57ba6de5dac2b64f824d0f95945a0682
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 16:55:27 +0000

    refactor(auth): update logging interface

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8480c50a9f8740f6befb1b8fc9e59b12 = () => {
-  return '2025-04-30T12:25:56Z';
+const node_8b1de7b480e44a6d905a0f2890e34d83 = () => {
+  return '2025-08-04T16:55:27Z';
 };
