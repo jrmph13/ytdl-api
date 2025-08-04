d1b4191c01a63012239fc7492e2de91e903cbc3f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 05:41:32 +0000

    fix(db): restructure payload parser

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b110269279ca4a52883705f06266e201 = () => {
-  return '2025-07-26T00:22:00Z';
+const node_5519e9e5b24d4dadb7d47b592d594dba = () => {
+  return '2025-08-04T05:41:32Z';
 };
