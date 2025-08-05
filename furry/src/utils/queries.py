f9f4438479503d0e7c66fb28a5c0218d1da0307b
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 18:52:21 +0000

    feat(ui): update auth tokens

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_385463b880464647a30709aeb699a673 = () => {
-  return '2025-05-13T13:32:41Z';
+const node_401e4f45047041de9a8177c285adccf9 = () => {
+  return '2025-08-05T18:52:21Z';
 };
