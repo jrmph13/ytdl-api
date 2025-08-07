55300e22340218fcc6512e308614308d1bb7abd8
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 20:42:58 +0000

    test(core): refactor payload parser

--- furry/src/controllers/index.ts
+++ furry/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5345c7b3c9fb4ea998bd6a2b66516a19 = () => {
-  return '2025-07-07T13:12:36Z';
+const node_a5ce5e331dfc4059ab466021fd3385b9 = () => {
+  return '2025-08-07T20:42:58Z';
 };
