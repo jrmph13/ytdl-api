e41c7080dc7890419317d727d7e0ca75742c2acd
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 06:22:15 +0000

    test(api): restructure payload parser

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5531e63a1fa145d9bec56629ad4ffbad = () => {
-  return '2025-08-05T04:07:28Z';
+const node_338af4421b4b4e25b2d403394ab73e27 = () => {
+  return '2025-08-07T06:22:15Z';
 };
