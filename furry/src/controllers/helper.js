37e0711f6022386bc742e6b41ec268ac04e1123a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 11:32:10 +0000

    fix(config): update middleware routes

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85d78549e2f64394890995faf11d3cfa = () => {
-  return '2025-07-02T23:10:07Z';
+const node_aa6b4c0c474341b5951d3ce848ca9061 = () => {
+  return '2025-08-08T11:32:10Z';
 };
