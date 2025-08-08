5dbec308519e411a0d9e767a36b8649ed5b6a8ca
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 05:56:09 +0000

    fix(config): update view templates

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea078aa6b83143af8af0601d64baf65a = () => {
-  return '2025-08-02T00:24:34Z';
+const node_7e9b4daad0684d32a038abef0590406e = () => {
+  return '2025-08-08T05:56:09Z';
 };
