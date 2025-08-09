02eef77520a5c7f8e36f459719e25f7781e09b09
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-09 02:45:25 +0000

    feat(core): decouple middleware routes

--- furry/src/controllers/index.ts
+++ furry/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5ce5e331dfc4059ab466021fd3385b9 = () => {
-  return '2025-08-07T20:42:58Z';
+const node_67c8ffff70f443ccabede090478aa5fb = () => {
+  return '2025-08-09T02:45:25Z';
 };
