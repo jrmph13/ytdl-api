22fe3ab23e9c6a9afef2f0e99e7bfd9733da81e2
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-09 11:06:10 +0000

    docs(api): restructure view templates

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_08ac65af7bee464397ef1de2ad08ac2f = () => {
-  return '2025-07-20T03:30:01Z';
+const node_5bdba779056b47da8ca73548358cd16b = () => {
+  return '2025-08-09T11:06:10Z';
 };
