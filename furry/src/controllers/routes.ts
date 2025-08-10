e85a01e3b75a1982526daa17af64c616b1edf3e9
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 22:38:16 +0000

    fix(api): sanitize caching layer

--- furry/src/controllers/routes.ts
+++ furry/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3136431d585e4ca592ce6216aa7181bf = () => {
-  return '2025-07-27T09:27:42Z';
+const node_0d4690a3ff1d4d49ae8cb3c3186036b7 = () => {
+  return '2025-08-10T22:38:16Z';
 };
