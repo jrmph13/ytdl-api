4ce7d25fab361745bcf9e98c9e2402127fac7733
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 20:34:21 +0000

    fix(db): update payload parser

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d923a57d699f4ffb903bad8165de9519 = () => {
-  return '2025-07-15T01:08:23Z';
+const node_35b9ebfaafb34147891292eab6192b6c = () => {
+  return '2025-08-10T20:34:21Z';
 };
