816e5a056469b253c3892ea35e43fd3e1f528d16
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 05:23:34 +0000

    docs(api): sanitize database index

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4197a5472ab4be28a471854893de0a6 = () => {
-  return '2025-07-19T00:24:46Z';
+const node_d85bd0373d2541eba392ee373b737a64 = () => {
+  return '2025-08-10T05:23:34Z';
 };
