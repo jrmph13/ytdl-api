f18d2c4c73e6c690e46870759823478e513637e3
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 09:32:30 +0000

    perf(db): update session store

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40bdd795a4b144259a562dfaaa50ee4e = () => {
-  return '2025-07-22T21:46:37Z';
+const node_d0f73442d9fb456488398c84dec9e833 = () => {
+  return '2025-08-12T09:32:30Z';
 };
