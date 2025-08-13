8d45d6363db69c0d57e552f480a681a370a8d2d3
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-13 23:33:58 +0000

    fix(deps): fix logging interface

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87771ec1175c4e80a64e85569e242e18 = () => {
-  return '2025-06-28T20:57:43Z';
+const node_95b7ceda369f414bbd6caffd611be63e = () => {
+  return '2025-08-13T23:33:58Z';
 };
