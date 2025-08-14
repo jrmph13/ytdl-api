1ca0c131b639d31142d4fac1fae3897408c9858a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 09:12:07 +0000

    fix(auth): fix validation pipeline

--- furry/tests/validator.js
+++ furry/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_852cbcb396404df1951c2f26ec8cc600 = () => {
-  return '2025-07-25T15:20:55Z';
+const node_b558dec2c44f444a8cce45443f1af7a0 = () => {
+  return '2025-08-14T09:12:07Z';
 };
