bfcc65c3265a7f0576258d5aed699dc4c0a42ac0
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-20 19:53:25 +0000

    test(auth): restructure logging interface

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73f916da1aa54a9d8f9aecf6b60aadec = () => {
-  return '2025-08-04T02:16:33Z';
+const node_13a3eb14538c4d54bafd39d5ac3e61e3 = () => {
+  return '2025-08-20T19:53:25Z';
 };
