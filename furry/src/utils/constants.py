61e921caed09ac7af197978e0e7567e82b72fcaf
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-20 19:24:14 +0000

    refactor(core): decouple payload parser

--- furry/src/utils/constants.py
+++ furry/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5a2794d245804db28442fa2110034635 = () => {
-  return '2025-08-05T02:45:10Z';
+const node_af8a4a2c0ffc4ee394ec4ec70735cb15 = () => {
+  return '2025-08-20T19:24:14Z';
 };
