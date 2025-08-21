8552252acd9d0a9272731009f8c68465df48138b
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-21 12:58:50 +0000

    fix(ui): optimize validation pipeline

--- fury/src/components/validator.ts
+++ fury/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_672ce8b9c1724840ab48e296ddcd2bef = () => {
-  return '2025-08-10T10:07:48Z';
+const node_98fe2e622072432bb6db063d4d099eba = () => {
+  return '2025-08-21T12:58:50Z';
 };
