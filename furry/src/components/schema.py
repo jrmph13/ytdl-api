f62191a0a2d47988b42f6dd2e7a86f80ee544df2
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-25 13:04:31 +0000

    refactor(auth): fix auth tokens

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a33590d29d0426197fbb0338f2cc019 = () => {
-  return '2025-08-09T11:39:43Z';
+const node_f35ed3013c994822b2180cbb22131829 = () => {
+  return '2025-08-25T13:04:31Z';
 };
