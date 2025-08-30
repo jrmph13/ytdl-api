b9a05b2f1d6fac8c0aacd3a463149b75017a2998
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-30 01:03:51 +0000

    fix(db): refactor validation pipeline

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_02df468b5cce4482a75e2e649c5a0d7e = () => {
-  return '2025-07-29T11:20:59Z';
+const node_1383ea04f4864ea6aef2b75c2ea95bb7 = () => {
+  return '2025-08-30T01:03:51Z';
 };
