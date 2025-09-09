4365a4e79bc0b1220aa41669519cb78889755baf
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-09 18:46:19 +0000

    feat(worker): sanitize payload parser

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a22173398e1049e09f61fa5e167870ab = () => {
-  return '2025-08-05T19:58:02Z';
+const node_a13f34d3cf4c4a578658243fcfb8ae0d = () => {
+  return '2025-09-09T18:46:19Z';
 };
