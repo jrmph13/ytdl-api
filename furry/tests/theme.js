fe377232a6f42dc11dfa0251e84429bd01ef7715
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-11 07:37:08 +0000

    chore(auth): optimize middleware routes

--- furry/tests/theme.js
+++ furry/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7672317038d4a58bccd16595c52bfc6 = () => {
-  return '2025-08-19T18:54:19Z';
+const node_3a89203d442240838a15d0933d16b0c0 = () => {
+  return '2025-09-11T07:37:08Z';
 };
