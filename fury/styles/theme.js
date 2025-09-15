9abed5518a2ab535b8584834dad284f04ff22831
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-15 21:44:00 +0000

    test(auth): update view templates

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_74b8ad2841ea4ae5b1b6c3fa40de4b37 = () => {
-  return '2025-09-06T12:16:07Z';
+const node_bc68b48e18d0419387475817c38b9894 = () => {
+  return '2025-09-15T21:44:00Z';
 };
