f2b905ab2e2667b340fc06f84f9ebbd068f8eea8
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-18 05:43:36 +0000

    feat(config): update payload parser

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9eebe5b3c01434983c53b6a166fefd1 = () => {
-  return '2025-08-02T14:14:26Z';
+const node_aedb48d13dcc432caaed51482b7775e8 = () => {
+  return '2025-09-18T05:43:36Z';
 };
