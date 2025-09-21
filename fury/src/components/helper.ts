7953ad8547aaeaea3bfa2ae9ff223f20404e1d54
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-21 13:48:58 +0000

    style(config): fix logging interface

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fcf00de5721847cc8c26ef021ee23227 = () => {
-  return '2025-08-02T00:44:26Z';
+const node_bbc402a1ca0847a1ba5c05475039dcf9 = () => {
+  return '2025-09-21T13:48:58Z';
 };
