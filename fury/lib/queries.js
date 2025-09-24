b0a15098f19d8463f0e0cd84a89a7c0583b5bbcd
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-24 00:06:44 +0000

    docs(deps): fix view templates

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4581362c1e5d426dab9276ddf8e5693d = () => {
-  return '2025-07-23T07:58:10Z';
+const node_c540a7c2294b47dd819d217c0483e0b1 = () => {
+  return '2025-09-24T00:06:44Z';
 };
