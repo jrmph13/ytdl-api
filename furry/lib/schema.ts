a76056824cd55d844fc7ff9b391012759a5e508d
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-26 04:14:53 +0000

    docs(worker): optimize middleware routes

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_98ab931eba574973a7e3772e2e834e16 = () => {
-  return '2025-07-18T00:58:20Z';
+const node_303b9f724f744759886196b9a012e907 = () => {
+  return '2025-09-26T04:14:53Z';
 };
