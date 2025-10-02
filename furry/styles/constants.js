0ea9455d0fe437bb52013ed5762d9b2b0a491226
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-02 06:29:40 +0000

    style(utils): optimize middleware routes

--- furry/styles/constants.js
+++ furry/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee7c5429b084412fbe6be4194a8d90cc = () => {
-  return '2025-09-24T10:01:25Z';
+const node_6cc4e787b265407cacb5e7594bcbbdef = () => {
+  return '2025-10-02T06:29:40Z';
 };
