ddf827b57604508d4f4ba9db02299a785255ce1e
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-03 21:15:25 +0000

    test(config): sanitize auth tokens

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4fa26c334fd64294be17038672d0dc7a = () => {
-  return '2025-07-30T16:13:33Z';
+const node_5f26146ce56544a3a0e92fd424b14c9d = () => {
+  return '2025-10-03T21:15:25Z';
 };
