314ef999cfe8a12e4772b2c179560ef358bb48f4
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-16 20:14:55 +0000

    chore(config): restructure caching layer

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5a91d11cf7b14adaaba26fc4630e1d20 = () => {
-  return '2025-06-15T17:09:24Z';
+const node_51b974676fea47b2b64a4a1f82b1fec7 = () => {
+  return '2025-10-16T20:14:55Z';
 };
