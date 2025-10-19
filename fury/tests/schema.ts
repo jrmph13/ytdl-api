b42ce5da7083e97b10d8253ce01dd60a74f5aaa6
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-19 05:43:05 +0000

    fix(auth): optimize database index

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d971a6647db64758b569f556111764e3 = () => {
-  return '2025-07-07T02:45:09Z';
+const node_1ee3f5e73c654b889ee4c914539df457 = () => {
+  return '2025-10-19T05:43:05Z';
 };
