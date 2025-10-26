f897eae855cde8e3751704aa6c821f20a5a40d2a
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-26 05:23:42 +0000

    chore(ui): integrate auth tokens

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f99fd8b9fdd48ff8517fc3a412d42f7 = () => {
-  return '2025-10-03T10:09:39Z';
+const node_ff2b544418c84b0d99582783ad09c704 = () => {
+  return '2025-10-26T05:23:42Z';
 };
