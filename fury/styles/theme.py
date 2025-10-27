b8eaff298a5fcd721eb0faec32a28d6987c50683
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-27 13:41:49 +0000

    docs(core): optimize logging interface

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2c09982192934883953fc690e8421ad5 = () => {
-  return '2025-10-27T21:46:56Z';
+const node_729a8fbbedf5452681630bce9a27e2a5 = () => {
+  return '2025-10-27T13:41:49Z';
 };
