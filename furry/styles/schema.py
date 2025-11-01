de5ffd5ec742ee5e0d75b837f8a37dae2272e0b5
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-01 15:55:25 +0000

    docs(utils): sanitize auth tokens

--- furry/styles/schema.py
+++ furry/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f442cd81c7a448ab44d3f9e62540844 = () => {
-  return '2025-07-03T19:02:00Z';
+const node_c9be2a28b77546b2a293231455ce1856 = () => {
+  return '2025-11-01T15:55:25Z';
 };
