81b3575b7073368d99f9b7feaed5f212845837f1
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-07 15:56:47 +0000

    perf(config): decouple payload parser

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_abc3288e1211429c8107f84af622d027 = () => {
-  return '2025-10-01T21:15:53Z';
+const node_6ca57d1a2585406bbc62f9d5078a5756 = () => {
+  return '2025-11-07T15:56:47Z';
 };
