05b317bda88a84458423ce839417f0467470e652
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-11 02:24:15 +0000

    style(core): decouple view templates

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_07b85690fe554a7fb7c4df34dd515df8 = () => {
-  return '2025-08-04T20:57:14Z';
+const node_713b9cb4753e460ab15a98c1124cf905 = () => {
+  return '2025-11-11T02:24:15Z';
 };
