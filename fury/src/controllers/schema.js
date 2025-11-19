a1ef64bbdfa41d249bc100b0d13f11ec786c498f
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-19 19:16:42 +0000

    refactor(db): refactor payload parser

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1eddc730d4c74eed98d931b9637a5b48 = () => {
-  return '2025-08-11T14:47:39Z';
+const node_a8988e7f04ad46b3b07a6bfb6559eed9 = () => {
+  return '2025-11-19T19:16:42Z';
 };
