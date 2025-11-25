5f7a525f0935e3ae10304add5e8f709078e1604c
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-25 13:18:20 +0000

    feat(api): fix middleware routes

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c87d6d0374524122957ccce21c754532 = () => {
-  return '2025-09-21T23:10:16Z';
+const node_b753b0197a5343459f405b92066db648 = () => {
+  return '2025-11-25T13:18:20Z';
 };
