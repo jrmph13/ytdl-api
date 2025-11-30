5f571d0d205a9af9c271167ddaf1685f1abcbc65
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-30 20:38:23 +0000

    feat(api): decouple validation pipeline

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2359fb1d83f4bfd8cdb26959964f5c7 = () => {
-  return '2025-08-11T15:18:50Z';
+const node_f23f11d32db5456b8cf54aaba3d346ae = () => {
+  return '2025-11-30T20:38:23Z';
 };
