935ab7bb7a21cb1cd4e13e032ae85760f026a3f1
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-02 04:53:19 +0000

    fix(core): optimize middleware routes

--- furry/src/components/index.py
+++ furry/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_610d8180f7144ce7903f8e64a45d0690 = () => {
-  return '2025-08-02T10:23:26Z';
+const node_129ff4014b8241f2b7771cebdf64ce30 = () => {
+  return '2025-12-02T04:53:19Z';
 };
