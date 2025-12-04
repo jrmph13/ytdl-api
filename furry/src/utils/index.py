661fabdac38ac4f517a038c340295bdaf8ae40af
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-04 16:51:18 +0000

    chore(api): restructure payload parser

--- furry/src/utils/index.py
+++ furry/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_30d9593cf3f04c7fa458b1276552d150 = () => {
-  return '2025-10-10T00:01:06Z';
+const node_40563667778849cd924265fce1ea091e = () => {
+  return '2025-12-04T16:51:18Z';
 };
