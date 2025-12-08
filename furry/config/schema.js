b97c8ed4528883eaf77ac3c8679467037d2d8679
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-08 00:11:49 +0000

    fix(auth): integrate payload parser

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4685ee202f1d4b44961e701fdb7ad04a = () => {
-  return '2025-08-20T16:23:40Z';
+const node_de45229474b740b6884b5ec70f0d95ee = () => {
+  return '2025-12-08T00:11:49Z';
 };
