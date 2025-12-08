0171d62794b84f82e6f86abadf7ea67b7ea87b92
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-08 11:00:11 +0000

    style(db): sanitize view templates

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cebdc42c7b9b43bcab414105e091d5dd = () => {
-  return '2025-08-12T14:14:03Z';
+const node_a2af6c7d583f452f83e6b44331001ff1 = () => {
+  return '2025-12-08T11:00:11Z';
 };
