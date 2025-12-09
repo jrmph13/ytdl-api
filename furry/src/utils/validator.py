1390d076be8ce5ee86197dfee10e62510ba56c1f
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-09 21:19:40 +0000

    refactor(api): sanitize middleware routes

--- furry/src/utils/validator.py
+++ furry/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_089e86dfde3744a0837305a8171821cd = () => {
-  return '2025-10-28T08:38:34Z';
+const node_b729c602392348abb1a83c6e530250ea = () => {
+  return '2025-12-09T21:19:40Z';
 };
