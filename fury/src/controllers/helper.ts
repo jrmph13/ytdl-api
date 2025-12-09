2755084fe15eec1ea2d717160fe6c87b82f83405
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-09 03:19:11 +0000

    fix(config): refactor database index

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ad9b104b6b14153a80822f236fec741 = () => {
-  return '2025-10-27T19:13:37Z';
+const node_16b6eb7e7932436eb63ae61b6082462e = () => {
+  return '2025-12-09T03:19:11Z';
 };
