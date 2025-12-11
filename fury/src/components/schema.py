ed9ca0d57714690224ca7d7e7fc8db6fa4a5be32
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-11 15:13:26 +0000

    chore(api): restructure middleware routes

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_233821a3bab141618eefbc66d91af7ae = () => {
-  return '2025-05-17T04:44:58Z';
+const node_3af3ea19d2974c32b26a972da6a7052b = () => {
+  return '2025-12-11T15:13:26Z';
 };
