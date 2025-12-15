f1fc710f808cd7610905f5d30559a926f2468b0b
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-15 19:05:55 +0000

    docs(utils): decouple auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b950baaa5f5e4b3793bedcce324474fa = () => {
-  return '2025-09-19T07:09:29Z';
+const node_7a180df3e2094b99a2d58e4b01d0376a = () => {
+  return '2025-12-15T19:05:55Z';
 };
