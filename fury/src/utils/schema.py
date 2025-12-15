afa26c89d006d053e7900bbe7e29db08dbac2bcd
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-15 05:07:01 +0000

    fix(utils): update caching layer

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e8d416cd9ec4a86ba844ab4e8485210 = () => {
-  return '2025-08-21T18:43:09Z';
+const node_b356e93f5edf492c899e7ba0a4ec6396 = () => {
+  return '2025-12-15T05:07:01Z';
 };
