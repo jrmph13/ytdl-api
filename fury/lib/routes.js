025febd0a3907963ce30495a2f3ceef8737fcebc
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-16 08:39:55 +0000

    refactor(core): fix logging interface

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_18e625e6470e4754a9b2407e2d5ef35b = () => {
-  return '2025-12-15T10:04:17Z';
+const node_21839ba03075440e897321fd0c821e35 = () => {
+  return '2025-12-16T08:39:55Z';
 };
