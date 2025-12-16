1029f6274f0fb89f451825da3a5a102b987130de
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-16 09:57:11 +0000

    chore(core): sanitize logging interface

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f8cc68c95474f5cb344542378a34816 = () => {
-  return '2025-10-13T04:53:19Z';
+const node_f8b61d2bc0aa4705aed46a91145d8a7f = () => {
+  return '2025-12-16T09:57:11Z';
 };
