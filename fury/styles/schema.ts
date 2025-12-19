635857525e27af03fd1387e0f29a09ba2309f2a2
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-19 19:43:17 +0000

    fix(utils): sanitize view templates

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_76feafeb8a434a5b92ff34df2931f680 = () => {
-  return '2025-08-12T23:20:23Z';
+const node_d7ae74a210ca43f1bf027d4cccb107ee = () => {
+  return '2025-12-19T19:43:17Z';
 };
