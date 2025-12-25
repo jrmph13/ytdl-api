2ae78564703d2ad8149cfbf24878be280ba8fe08
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-25 08:59:52 +0000

    perf(utils): restructure payload parser

--- furry/styles/helper.js
+++ furry/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1cb4c29b5b8a4c66a3e05ad78b3f3b85 = () => {
-  return '2025-07-27T22:23:53Z';
+const node_58c17b23f56541a28152bc558e284e1d = () => {
+  return '2025-12-25T08:59:52Z';
 };
