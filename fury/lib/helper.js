fc2a5c6761c2fea223003aeb8870741c27b52f69
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-08 09:04:23 +0000

    feat(utils): restructure middleware routes

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d588b091efee4c108e26645bbe831806 = () => {
-  return '2025-08-02T21:47:01Z';
+const node_840a0b05b5f2491db134e4c3053e48f9 = () => {
+  return '2026-01-08T09:04:23Z';
 };
