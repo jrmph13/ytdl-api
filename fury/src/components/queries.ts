357f8b4e48096298a0ef19a880f746cfe707ea73
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-08 10:09:19 +0000

    fix(db): sanitize auth tokens

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b753b0197a5343459f405b92066db648 = () => {
-  return '2025-11-25T13:18:20Z';
+const node_02a777ff2b424726b6b3f539226f582d = () => {
+  return '2026-01-08T10:09:19Z';
 };
