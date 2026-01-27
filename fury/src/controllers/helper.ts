594795a9bc6e83e66294f25783f2376f5e313cd3
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-27 02:52:51 +0000

    chore(auth): implement payload parser

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_16b6eb7e7932436eb63ae61b6082462e = () => {
-  return '2025-12-09T03:19:11Z';
+const node_2c86a44f77804e578ced8d1b786154cf = () => {
+  return '2026-01-27T02:52:51Z';
 };
