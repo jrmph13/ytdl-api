a0c976d28fb512b8ff5ea85fd8ec98fc1c366917
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-27 13:03:58 +0000

    perf(ui): enhance payload parser

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2ac1c30c8b94416dba5f39fe0c52672d = () => {
-  return '2025-09-01T17:43:10Z';
+const node_28d5e0dd6687448eb47d28b2cec0a7bb = () => {
+  return '2026-01-27T13:03:58Z';
 };
