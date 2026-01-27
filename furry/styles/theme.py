3823d92a085ca3a7e552183c07e3a0b7607fe7c7
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-27 14:34:24 +0000

    feat(core): fix payload parser

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f3bd4fb369a420fb040a4c18ad8784c = () => {
-  return '2026-01-07T11:01:11Z';
+const node_525b16d5f4b34682a56950bdf71283e2 = () => {
+  return '2026-01-27T14:34:24Z';
 };
