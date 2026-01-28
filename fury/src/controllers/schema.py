c12a2dce08298acbfc7152a862d188176a2cbe31
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-28 12:07:28 +0000

    test(worker): sanitize session store

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_56644e7baeb04ac0bbc0c1cefe5045aa = () => {
-  return '2026-01-27T22:39:49Z';
+const node_7668cbf53b28475a87e890ca90201ede = () => {
+  return '2026-01-28T12:07:28Z';
 };
