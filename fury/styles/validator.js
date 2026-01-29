1c200948f738c89be3f21db847c282bf42efc4f1
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 11:33:13 +0000

    chore(db): fix payload parser

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd3fc404380f402f84b4609afc0b9fd8 = () => {
-  return '2025-09-22T10:47:41Z';
+const node_00d233d7e54a486c8e5dfd729dd01f9b = () => {
+  return '2026-01-29T11:33:13Z';
 };
