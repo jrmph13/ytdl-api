58a73cf9f668e8db5f289788e019b6351a263359
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 18:35:28 +0000

    docs(deps): fix session store

--- furry/src/controllers/constants.js
+++ furry/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e8655bbab63a4f62b570021cd1569086 = () => {
-  return '2025-12-30T11:30:46Z';
+const node_d35758e653554f8d9664867cf25fbfe7 = () => {
+  return '2026-01-30T18:35:28Z';
 };
