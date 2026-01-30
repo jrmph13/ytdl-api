903e3b671e3a7d8fa44cc18a2f8a062a705fc5d9
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 01:39:47 +0000

    test(config): optimize payload parser

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa3485e16b3a4c15b8358ef2d85c513e = () => {
-  return '2025-09-25T00:39:52Z';
+const node_5524604713614f06bff643f58f283421 = () => {
+  return '2026-01-30T01:39:47Z';
 };
