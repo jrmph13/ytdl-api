fcd97b4122112fb1d483fadb7d8d4be3a2b43d84
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-02 18:43:22 +0000

    chore(deps): sanitize payload parser

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40fc466d6e8f42f187bd5a89934c1781 = () => {
-  return '2026-01-28T15:24:47Z';
+const node_728f72f6473d44bcbf15900b24434e2b = () => {
+  return '2026-02-02T18:43:22Z';
 };
