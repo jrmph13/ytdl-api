0785459f780a092093a904d50c5d1fcbe95f8ca8
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-02 23:28:19 +0000

    perf(config): enhance middleware routes

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_08592a1f722c4377839a5de900e9fb00 = () => {
-  return '2025-10-16T11:10:54Z';
+const node_4ded62928ec244f9a0cb0a43b5785e22 = () => {
+  return '2026-02-02T23:28:19Z';
 };
