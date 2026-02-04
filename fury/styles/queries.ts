f95104f0694baabdbdab908129834bdecd4cb23c
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 15:06:13 +0000

    fix(auth): update caching layer

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c989f07a63140dd8d973f45cb9fbd7f = () => {
-  return '2025-12-28T06:31:11Z';
+const node_32e6462c4a5e43f39646903f23276e46 = () => {
+  return '2026-02-04T15:06:13Z';
 };
