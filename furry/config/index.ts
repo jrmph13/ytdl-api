7cf61e5fc32623d27b0b11d73852955f99f131ea
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-09 09:10:07 +0000

    perf(db): restructure session store

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b426f3606ed440a3a043700b41dd5898 = () => {
-  return '2025-12-16T09:30:36Z';
+const node_034e47ea283e4213b2177b239fecc032 = () => {
+  return '2026-02-09T09:10:07Z';
 };
