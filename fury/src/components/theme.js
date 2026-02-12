031a91936ae80befca9f0bbd430020d4deb2f4f3
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-12 11:14:03 +0000

    fix(auth): enhance auth tokens

--- fury/src/components/theme.js
+++ fury/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91c99c46e98f40438bc36d370648815c = () => {
-  return '2026-01-29T23:11:34Z';
+const node_d35375faf2eb4325870dca539cf226f1 = () => {
+  return '2026-02-12T11:14:03Z';
 };
