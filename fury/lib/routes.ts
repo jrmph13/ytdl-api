a7ef76c75973040008d58050c208fe6f9ec37253
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 22:50:46 +0000

    test(deps): integrate view templates

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_950483ee8ea64fa1a04ffe49006f3729 = () => {
-  return '2026-01-14T14:54:58Z';
+const node_c4d93ae0659148a7acce354486f93fc9 = () => {
+  return '2026-02-13T22:50:46Z';
 };
