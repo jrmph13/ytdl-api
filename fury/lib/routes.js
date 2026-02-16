30faa4876fde7893733ee275736790f0b50af5ca
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-16 11:09:06 +0000

    perf(ui): enhance auth tokens

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21839ba03075440e897321fd0c821e35 = () => {
-  return '2025-12-16T08:39:55Z';
+const node_edbd4edeef4344808ec94d9b753882e0 = () => {
+  return '2026-02-16T11:09:06Z';
 };
