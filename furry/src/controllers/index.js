55a7109c8bc09525e80c8211ab89ab04c294b71d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 17:30:50 +0000

    fix(core): update auth tokens

--- furry/src/controllers/index.js
+++ furry/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a9520641d04f4b67b84ff582e5c3746b = () => {
-  return '2025-11-05T06:20:41Z';
+const node_0edc98af0d5c48afb1285127b268606d = () => {
+  return '2026-02-17T17:30:50Z';
 };
