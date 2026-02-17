d6dd675afed03bf81129abad690d4d921424db79
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 14:11:07 +0000

    feat(core): decouple auth tokens

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8322578746d445feb6da169b11480874 = () => {
-  return '2026-02-09T19:31:04Z';
+const node_52073484b556485fb21ab0cd64a75126 = () => {
+  return '2026-02-17T14:11:07Z';
 };
