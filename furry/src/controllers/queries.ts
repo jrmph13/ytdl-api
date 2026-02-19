6ea94fb742c3515558634e5434450903e56cbdd8
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 18:19:45 +0000

    refactor(db): fix session store

--- furry/src/controllers/queries.ts
+++ furry/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc5c00f8b4064082829fad03a8def6c3 = () => {
-  return '2026-02-10T20:42:06Z';
+const node_ad2e736146de44f48297f7bdcd2556fc = () => {
+  return '2026-02-19T18:19:45Z';
 };
