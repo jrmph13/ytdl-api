ba0bec5a68a2a63333fcfa9114742a5a37c984a7
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 03:49:50 +0000

    feat(ui): decouple middleware routes

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b7084fe867a74a098ad0b3eba964cfae = () => {
-  return '2025-12-12T15:57:50Z';
+const node_26aa1abe3fab4903ac96f0216d51ae26 = () => {
+  return '2026-02-22T03:49:50Z';
 };
