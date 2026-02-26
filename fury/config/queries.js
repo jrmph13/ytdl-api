f1e33349549164adfcc86e59a3e9f9d07b348792
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 00:25:10 +0000

    docs(config): update middleware routes

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_917439d2a06c4aeca4a43cf1c9221d48 = () => {
-  return '2025-12-16T05:20:48Z';
+const node_963bc3c2459f4bb39116491ee1c23a60 = () => {
+  return '2026-02-26T00:25:10Z';
 };
