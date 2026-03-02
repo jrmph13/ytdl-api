1ca09ed83a2a132c2116118360f14712f0934f70
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-02 06:36:25 +0000

    feat(auth): restructure middleware routes

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57592f78c4c948db91630fdf921763bb = () => {
-  return '2025-06-12T06:43:02Z';
+const node_a0cdbea977434feaa38b6a6f78c3cb08 = () => {
+  return '2026-03-02T06:36:25Z';
 };
