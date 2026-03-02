2df874b4cf65a1fc7b6688e51521871d3a1d107b
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-02 10:16:54 +0000

    test(core): fix middleware routes

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b3f4ceada134efc8b1958a726da7deb = () => {
-  return '2026-01-14T19:12:35Z';
+const node_21ba7e519cd449f6b9c6f90bc6652a92 = () => {
+  return '2026-03-02T10:16:54Z';
 };
