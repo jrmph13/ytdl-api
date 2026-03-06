c233a6f6704e5dd15b17f96a11027c1912cff7d6
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-06 16:26:07 +0000

    refactor(config): decouple session store

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_edbd4edeef4344808ec94d9b753882e0 = () => {
-  return '2026-02-16T11:09:06Z';
+const node_6b16261878a24fd9a48733323b05a8f5 = () => {
+  return '2026-03-06T16:26:07Z';
 };
