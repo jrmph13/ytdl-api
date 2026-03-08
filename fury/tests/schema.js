1163c69f81f586e89c33b315941035aec1d5ba1e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 03:16:22 +0000

    chore(api): restructure view templates

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_34835c92ae7b4551ad08f6cca3ade833 = () => {
-  return '2026-02-06T13:28:37Z';
+const node_716cf1eb2dfa49dba8d538fe2fadaaa1 = () => {
+  return '2026-03-08T03:16:22Z';
 };
