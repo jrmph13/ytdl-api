1556e1e71a976c0f2edfdaab02811ff1ae0db16d
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 06:49:36 +0000

    chore(core): fix middleware routes

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f677eaa1e4648208ea7ef1717942b82 = () => {
-  return '2026-02-25T06:15:35Z';
+const node_968ddeedb536415284e2ed2be4f95e7c = () => {
+  return '2026-03-08T06:49:36Z';
 };
