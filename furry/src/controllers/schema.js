d6e0b8eed3cac8df75ad2cdde1709df326212577
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-17 11:40:57 +0000

    feat(worker): fix database index

--- furry/src/controllers/schema.js
+++ furry/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ae1d3ae81f174e2ab64f2aac1e93d778 = () => {
-  return '2025-12-08T17:27:08Z';
+const node_c0d68d886e7f40b480862a496e970d22 = () => {
+  return '2026-03-17T11:40:57Z';
 };
