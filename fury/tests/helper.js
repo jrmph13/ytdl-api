783fc510dfd2359026440e31089ce658bde7cb88
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-18 22:32:30 +0000

    refactor(ui): refactor logging interface

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6e17bf1eb5d494e90f330a552871391 = () => {
-  return '2026-02-14T00:37:54Z';
+const node_f244d7400a8e454082e50d5ce4509d56 = () => {
+  return '2026-03-18T22:32:30Z';
 };
