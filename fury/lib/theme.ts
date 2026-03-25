013d51ef7c14d06789c571e07a3c3415c8ee24ab
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 10:52:05 +0000

    fix(worker): refactor view templates

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b575dede96e545f88317565684c94444 = () => {
-  return '2026-01-26T11:58:28Z';
+const node_1e414b605a574980b0ba7d55fd3759e1 = () => {
+  return '2026-03-25T10:52:05Z';
 };
