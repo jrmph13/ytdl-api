2d6772e66ef25658542c75ba8d7fd3bbdb7d296a
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 17:40:10 +0000

    perf(core): sanitize logging interface

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57bd4e532e6048558fe4b474ee5b0d07 = () => {
-  return '2026-03-04T19:29:08Z';
+const node_a179e9374e464dd4a5016e4dc5f29971 = () => {
+  return '2026-03-25T17:40:10Z';
 };
