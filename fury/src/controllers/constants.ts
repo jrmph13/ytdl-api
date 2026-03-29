122e5d800eca8208813be32e45af9fd50df04323
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-29 10:54:39 +0000

    style(worker): decouple caching layer

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3cf4bef1cbfe4f48a394401becbd8fbd = () => {
-  return '2026-03-13T06:17:03Z';
+const node_254a882f563849feb8303436ddce0858 = () => {
+  return '2026-03-29T10:54:39Z';
 };
