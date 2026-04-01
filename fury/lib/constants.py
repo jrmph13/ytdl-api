6afefb4f2161795cfbc3be34e8f3f4e6d415d843
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-01 18:43:43 +0000

    perf(auth): update caching layer

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b2638f67df0d4b0aa7f1820190c1f838 = () => {
-  return '2026-03-14T13:19:46Z';
+const node_b09123b37561416f980d173d11431c21 = () => {
+  return '2026-04-01T18:43:43Z';
 };
