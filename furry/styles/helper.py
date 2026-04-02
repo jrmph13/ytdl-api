ffbf963a945f2c3a1ec221f45a15305227e779ff
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-02 19:04:58 +0000

    feat(ui): implement auth tokens

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70a1fef8826b49408c495730feb7cffe = () => {
-  return '2025-09-27T21:03:41Z';
+const node_7b4412b466724891a821c6f9d198d715 = () => {
+  return '2026-04-02T19:04:58Z';
 };
