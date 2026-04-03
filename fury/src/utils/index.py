a5df0315493db842e9ef81fc1bd05cbd6908ed8f
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 07:36:11 +0000

    style(worker): restructure payload parser

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dac46f9a28994f149bd389d887dd77de = () => {
-  return '2026-02-20T06:51:33Z';
+const node_21bfbabc923c496c801a2c2f0ea24816 = () => {
+  return '2026-04-03T07:36:11Z';
 };
