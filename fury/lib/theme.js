ca17e0935e8cb0fc234fe84f666c9f8423f8d7c6
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-10 01:52:20 +0000

    feat(api): integrate payload parser

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f40c3e6be39441d5b781cee803ffa3e3 = () => {
-  return '2026-03-14T08:03:50Z';
+const node_974c1bafffad4913830e16d11a9b08db = () => {
+  return '2026-04-10T01:52:20Z';
 };
