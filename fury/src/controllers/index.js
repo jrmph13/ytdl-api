c99aff1d52f14167b6e266632d3511d9e6571c54
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 01:26:34 +0000

    feat(config): fix auth tokens

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_54021733ea1647e98bc6c7e9e5045d0b = () => {
-  return '2026-02-09T00:52:10Z';
+const node_6b5ffff15d2c49bc8663f74091a1bd74 = () => {
+  return '2026-04-14T01:26:34Z';
 };
