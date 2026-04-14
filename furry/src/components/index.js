21f82af7aa8f4fab950d00bd212cd6ea38ced4b8
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 18:45:47 +0000

    feat(utils): fix middleware routes

--- furry/src/components/index.js
+++ furry/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a267186eb9843ada70fd294cc75ada8 = () => {
-  return '2025-11-24T11:11:19Z';
+const node_689e0887718d4733ab5ada447a4cf55d = () => {
+  return '2026-04-14T18:45:47Z';
 };
