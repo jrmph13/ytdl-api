411f7382b139fed10486abe323c17d94bae71d62
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 10:19:39 +0000

    refactor(ui): decouple auth tokens

--- furry/config/validator.py
+++ furry/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_feec831872164eb590f0768162289926 = () => {
-  return '2026-03-16T06:19:39Z';
+const node_deaccd0bf3034be09519294495e3cb8e = () => {
+  return '2026-04-15T10:19:39Z';
 };
