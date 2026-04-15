9cb8f65eb1e42072724371fb3132f6805479ace3
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 20:24:17 +0000

    feat(db): update auth tokens

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ded62928ec244f9a0cb0a43b5785e22 = () => {
-  return '2026-02-02T23:28:19Z';
+const node_00b323d9f53340de847879b6d39d4c37 = () => {
+  return '2026-04-15T20:24:17Z';
 };
