0f4dcbd70990ca1f4544d0b04303e0e2ae8eafb2
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 00:32:50 +0000

    docs(api): update session store

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa1507220ffc416bb4cd3896e39e536c = () => {
-  return '2025-08-03T18:28:36Z';
+const node_287f180272c448fe8f210087c48d1f3a = () => {
+  return '2026-04-17T00:32:50Z';
 };
