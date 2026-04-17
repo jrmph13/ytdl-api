9291c0d88df729cab36c47d4b3a699ad004bc9ae
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 23:34:14 +0000

    style(api): fix session store

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5e41a53b84ec4fce88c33c8003562433 = () => {
-  return '2025-07-15T23:29:17Z';
+const node_62f9836ccacd47ab90baf5472bb73c53 = () => {
+  return '2026-04-17T23:34:14Z';
 };
