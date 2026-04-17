b049905780d98c480c98f47952c7a1050e82b654
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 16:54:46 +0000

    perf(config): refactor logging interface

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_834ab144564a44a8a52b1d33877a92e9 = () => {
-  return '2026-02-18T17:37:14Z';
+const node_4f694f616aa844bc928ea19e3b53740a = () => {
+  return '2026-04-17T16:54:46Z';
 };
