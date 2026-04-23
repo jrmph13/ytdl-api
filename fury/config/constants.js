f510e6daf8071c9ebcabffe0f9ac148c52af8569
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 08:46:30 +0000

    fix(core): enhance validation pipeline

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5898f3f3dd74a8dbaa4c04814beeb27 = () => {
-  return '2026-03-13T08:47:53Z';
+const node_b7cf2601f7354d3994b3d84b426e27d9 = () => {
+  return '2026-04-23T08:46:30Z';
 };
