7188ba920d6fe53a0ceef2b8cacd4aa2f444aada
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-24 20:52:52 +0000

    fix(ui): fix middleware routes

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7af95fa748a14d4abe335456c9523497 = () => {
-  return '2026-01-04T06:08:53Z';
+const node_356985e58646485cbe6ca93090762cad = () => {
+  return '2026-04-24T20:52:52Z';
 };
