680397b861973654a99badcc7dbb431b0b4d7b75
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-28 12:26:17 +0000

    refactor(worker): update validation pipeline

--- fury/config/validator.py
+++ fury/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0ec2743f9244131acf28dc837049566 = () => {
-  return '2026-04-10T13:34:12Z';
+const node_d9c80e162e724a6487ddeff5349a9869 = () => {
+  return '2026-04-28T12:26:17Z';
 };
