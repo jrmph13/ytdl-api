205c26fa94f96e1ce234f74af45ff41558174098
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 22:52:36 +0000

    test(worker): sanitize auth tokens

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dfd619dd9526426cb45f45a2e2e85088 = () => {
-  return '2026-02-16T21:48:43Z';
+const node_32dcbaaa3e4d4cdcb446a45995a3f237 = () => {
+  return '2026-05-02T22:52:36Z';
 };
