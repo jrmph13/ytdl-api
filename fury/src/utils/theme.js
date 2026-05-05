d13d2bb84aceee52ee02195dc07c6f8ab4380497
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-05 05:50:41 +0000

    test(worker): fix caching layer

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e798fa7f98f74702a4147dc52df396f3 = () => {
-  return '2026-02-02T05:43:46Z';
+const node_d649d37e21b54a25ad0e4bd03306f3a2 = () => {
+  return '2026-05-05T05:50:41Z';
 };
