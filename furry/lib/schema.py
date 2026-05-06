d6ef921bc9a5c0bf35a9d34ce284c14912fc6b85
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 02:06:57 +0000

    refactor(auth): decouple view templates

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4fe51f2790fc4cbba4486940a32a707c = () => {
-  return '2026-04-16T11:44:19Z';
+const node_bfb470120d9d474d8558e913e4d76170 = () => {
+  return '2026-05-06T02:06:57Z';
 };
