e79f36fcddb58d384cca23b656302e6ccf58e41f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 04:48:26 +0000

    test(deps): update view templates

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f433788912a7495bac6dfb66da169529 = () => {
-  return '2026-03-13T15:36:03Z';
+const node_f371d1d777ab49ecb2d456090693d9f6 = () => {
+  return '2026-05-08T04:48:26Z';
 };
