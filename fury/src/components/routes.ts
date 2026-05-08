19cbe234a8f450caaaac867ece45898cd3f29fe7
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 04:02:36 +0000

    docs(core): enhance auth tokens

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c0a15904ff2c444bad74880f7b0f53cd = () => {
-  return '2026-03-25T03:16:40Z';
+const node_4a1bc9066df244a1a6c613c3a769b2f8 = () => {
+  return '2026-05-08T04:02:36Z';
 };
