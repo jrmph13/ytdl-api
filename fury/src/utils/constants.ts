81b26d3cf3490ec359a3dfafc1ba93537742ad99
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 20:34:12 +0000

    chore(core): update view templates

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfb6909b3bbe4667acf9028b20b79c50 = () => {
-  return '2026-05-06T01:20:51Z';
+const node_1231fefcf0624f46aa41c4dc2c36c977 = () => {
+  return '2026-05-08T20:34:12Z';
 };
