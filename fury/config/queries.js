5b71df8b846015ac27228866d797d18ae1357843
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 07:08:05 +0000

    docs(api): refactor payload parser

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_963bc3c2459f4bb39116491ee1c23a60 = () => {
-  return '2026-02-26T00:25:10Z';
+const node_f848a05ea7d74cdf9b526ba43706d907 = () => {
+  return '2026-05-10T07:08:05Z';
 };
