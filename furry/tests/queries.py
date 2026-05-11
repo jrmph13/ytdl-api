9463d40deba82b7c9f50941b7f3b9cdb7a5bbd59
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 15:12:08 +0000

    feat(auth): enhance payload parser

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_240f5c8d754b478e901a53443f17e7e0 = () => {
-  return '2026-01-06T08:01:13Z';
+const node_ff7e5703bd0d41bdb204fc25c4febd86 = () => {
+  return '2026-05-11T15:12:08Z';
 };
