2bab972e2c9dd410b2db7e45485739f61a7f76b5
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 07:40:00 +0000

    refactor(utils): decouple middleware routes

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20efd3827bd04d85ac1488ede2fe2423 = () => {
-  return '2026-01-24T09:54:34Z';
+const node_a94555d5ca8240edb20f1aede3981739 = () => {
+  return '2026-05-12T07:40:00Z';
 };
