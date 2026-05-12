ea66d06d2de098bb74783c6bee54e4d6832bdedb
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 14:26:11 +0000

    chore(auth): optimize database index

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de38e75837084a84a497e1877c0cafb2 = () => {
-  return '2026-04-14T00:10:41Z';
+const node_791fad6a8cab4accbadc28c4985196b1 = () => {
+  return '2026-05-12T14:26:11Z';
 };
