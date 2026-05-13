b881754464aa6e5b9c3cb17e73adab1b2d6e3fb2
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 05:05:12 +0000

    chore(api): decouple session store

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7e710bac9f34d309255ad6c6ee2fe8a = () => {
-  return '2026-03-31T01:23:14Z';
+const node_d510ed4cbc254e0fb92fdd20a5df4049 = () => {
+  return '2026-05-13T05:05:12Z';
 };
