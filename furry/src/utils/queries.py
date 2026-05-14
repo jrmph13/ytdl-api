04bcd1893fc78bec25495608dc9f02a1296f95f7
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 22:21:29 +0000

    feat(worker): restructure auth tokens

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2854e4151cb2448e81abf97e84ec90b6 = () => {
-  return '2025-09-17T14:51:32Z';
+const node_a0c696eb142b4439aa247aa7cca91aee = () => {
+  return '2026-05-14T22:21:29Z';
 };
