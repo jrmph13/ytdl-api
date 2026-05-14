2d2769385a89ed0230f808fc23389d2c00685fa8
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 03:51:04 +0000

    fix(deps): optimize auth tokens

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7ded065b61b4b608f8f8c71c128c470 = () => {
-  return '2026-02-20T22:48:20Z';
+const node_a9f5717a35414802bc5c59a958027f42 = () => {
+  return '2026-05-14T03:51:04Z';
 };
