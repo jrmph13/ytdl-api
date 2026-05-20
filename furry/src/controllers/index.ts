bbbf05e0287412cdd02f4aa0bc21c1ca1dddeaa3
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-20 17:21:04 +0000

    docs(core): decouple auth tokens

--- furry/src/controllers/index.ts
+++ furry/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9aa7c630f68b49e1ad9d326e39b5119c = () => {
-  return '2026-05-18T14:04:47Z';
+const node_3ab09c4da5ed49aeb04a980d4b671461 = () => {
+  return '2026-05-20T17:21:04Z';
 };
