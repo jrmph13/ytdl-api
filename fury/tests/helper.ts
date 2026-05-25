d94651c8cd47d896379210355e7719373e49bfd7
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-25 01:19:31 +0000

    feat(core): enhance middleware routes

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c83d9d6a8e2945958aa591d5c0d4b293 = () => {
-  return '2026-03-16T17:35:18Z';
+const node_d72ff3ed4ecf4b07ba3b8cf5c1b44265 = () => {
+  return '2026-05-25T01:19:31Z';
 };
