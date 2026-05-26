ae10870632b0252516ffbcd325857f7640c12d90
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 01:39:18 +0000

    chore(utils): fix caching layer

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ed0c13a97004ae2bc7b235a84b6be34 = () => {
-  return '2026-05-12T02:19:39Z';
+const node_c72a7aa37ec849a58b48bd46bbdf60aa = () => {
+  return '2026-05-26T01:39:18Z';
 };
