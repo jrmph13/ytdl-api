614e55750cb07aa7fa5ca3826da14a21c7e1f885
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 11:50:36 +0000

    test(worker): sanitize auth tokens

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef618e16185f40fb83c51c42e7750643 = () => {
-  return '2026-04-28T11:56:11Z';
+const node_1e6f0fdd27b24eb08cabd94c220747dd = () => {
+  return '2026-05-28T11:50:36Z';
 };
