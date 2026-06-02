6cbe463c52a9ce1f02a6872f10002c40e4737652
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 00:59:36 +0000

    fix(ui): optimize auth tokens

--- furry/src/utils/queries.ts
+++ furry/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5acaf55cd087447a80c098ac9cf5d603 = () => {
-  return '2026-05-07T04:54:15Z';
+const node_a9bb76de73f943499403e192e4db71b6 = () => {
+  return '2026-06-02T00:59:36Z';
 };
