c123da1723031b4081136d1ac6e2b3d89a42d4a4
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 05:12:07 +0000

    chore(worker): restructure session store

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6b5ffff15d2c49bc8663f74091a1bd74 = () => {
-  return '2026-04-14T01:26:34Z';
+const node_c4ab574dd9fe4a499585c9facec6e121 = () => {
+  return '2026-06-02T05:12:07Z';
 };
