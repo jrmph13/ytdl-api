a1009d9139acb1943590ab8ee28d4617742ed952
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 21:14:48 +0000

    feat(worker): refactor session store

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f8898258a68f406fa29324f40c1484ae = () => {
-  return '2026-02-05T22:54:03Z';
+const node_955da71851de4d979e7e450ed8a130e4 = () => {
+  return '2026-06-02T21:14:48Z';
 };
