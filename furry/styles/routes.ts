76611f05dc5a1882c36abd638954a2d6bb9922ca
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 21:26:28 +0000

    fix(core): enhance auth tokens

--- furry/styles/routes.ts
+++ furry/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8a802b931f144ae4bff69effdd964c5a = () => {
-  return '2026-04-03T15:37:26Z';
+const node_9e4d59cc367545279d331de88701976e = () => {
+  return '2026-06-03T21:26:28Z';
 };
