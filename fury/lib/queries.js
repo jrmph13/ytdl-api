60654d0d620f20d57e827503ce221ddd48ecccdd
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 11:27:33 +0000

    perf(deps): refactor middleware routes

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d1b1a341ec2e448f8fb810f858dca214 = () => {
-  return '2026-05-28T08:07:46Z';
+const node_73494050800f4a879cab4f22116f51ea = () => {
+  return '2026-06-03T11:27:33Z';
 };
