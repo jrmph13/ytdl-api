67987b03d70fea2e3c3cb2e913374078c8e7744a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 13:59:50 +0000

    style(core): integrate payload parser

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_583035fd89e446a194a44228525e2aea = () => {
-  return '2026-05-08T17:05:12Z';
+const node_205f389e33b74aa69ea61534f1367520 = () => {
+  return '2026-06-03T13:59:50Z';
 };
