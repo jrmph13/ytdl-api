2224ea7f7bcba9666bbe52e45d901b783cab87e8
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 17:00:51 +0000

    feat(db): enhance database index

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_552d62ce7bc04bcf90f98779e23cb10a = () => {
-  return '2026-05-28T23:16:29Z';
+const node_37e12ac01ab642899e7395c4cfe8d36b = () => {
+  return '2026-06-03T17:00:51Z';
 };
