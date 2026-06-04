0feb022c6c210ba8ed8aef94917d3ad3d731f9fb
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 23:53:44 +0000

    fix(api): update middleware routes

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c72a7aa37ec849a58b48bd46bbdf60aa = () => {
-  return '2026-05-26T01:39:18Z';
+const node_c8a9bbeb429249b7a98038a5a4c9f02d = () => {
+  return '2026-06-04T23:53:44Z';
 };
