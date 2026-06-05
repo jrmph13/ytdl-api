ab7e7c6602aa50d4d601621f9c6ae4572b60f6b8
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 01:37:01 +0000

    fix(worker): sanitize middleware routes

--- furry/tests/routes.js
+++ furry/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e2fae73320d4d7c8c5267fdb55528fb = () => {
-  return '2026-06-03T17:25:42Z';
+const node_c50e3c5eb30b4f47ab9d938ac9c860ac = () => {
+  return '2026-06-05T01:37:01Z';
 };
