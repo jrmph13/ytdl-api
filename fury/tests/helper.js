1bd84b965e74583c1116b55274bb41a64342ff93
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 00:22:58 +0000

    fix(worker): sanitize auth tokens

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f244d7400a8e454082e50d5ce4509d56 = () => {
-  return '2026-03-18T22:32:30Z';
+const node_82ad36f433064253aa4ba16b2eaed7d7 = () => {
+  return '2026-06-05T00:22:58Z';
 };
