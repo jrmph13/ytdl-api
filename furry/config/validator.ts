c11a4bff9dcc836151f01cd9a3bfa9d01bc766d7
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 05:38:25 +0000

    fix(core): sanitize validation pipeline

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cfaca6cf07064c8685033c08a3aac91f = () => {
-  return '2026-05-20T22:11:13Z';
+const node_d721c5c667e74bd890e13adac8ad5a33 = () => {
+  return '2026-06-05T05:38:25Z';
 };
