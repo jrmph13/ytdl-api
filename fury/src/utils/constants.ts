5a6ff3d134f38438a27cecb45bb3a8b13a67b8cc
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 21:41:08 +0000

    chore(core): decouple session store

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1231fefcf0624f46aa41c4dc2c36c977 = () => {
-  return '2026-05-08T20:34:12Z';
+const node_6d32dc2a7a48451d96a8096327d90228 = () => {
+  return '2026-06-06T21:41:08Z';
 };
