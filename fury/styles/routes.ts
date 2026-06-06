bc6f017f87792bdc3f6a0f5f95da5a969a1035d9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 01:42:00 +0000

    feat(core): sanitize auth tokens

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7b15abad9a64ffa912212981bf07245 = () => {
-  return '2026-06-02T07:08:52Z';
+const node_c4be142a3b51429d917e1458e8fdcfb1 = () => {
+  return '2026-06-06T01:42:00Z';
 };
