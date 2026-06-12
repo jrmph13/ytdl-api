a451b4f40a28b1017ea33f2018658ef30c20a649
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 12:08:50 +0000

    fix(utils): integrate auth tokens

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_511d32cb7a664db4b4178bd5b778484f = () => {
-  return '2026-06-04T02:08:04Z';
+const node_d287e92d72864053a0540fcc5f07fb17 = () => {
+  return '2026-06-12T12:08:50Z';
 };
