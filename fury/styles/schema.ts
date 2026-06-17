a80eac845fb4b5594d95ac202ce88e47526c0749
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-17 04:14:24 +0000

    chore(utils): implement middleware routes

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d89a1a6a68f3414096b14d90b49be0a0 = () => {
-  return '2026-05-10T07:14:29Z';
+const node_9b8264949b2a49979fc03d7ef1e30ede = () => {
+  return '2026-06-17T04:14:24Z';
 };
