e21f38acb9d2a944d79d20aa1e1b5f006a44540c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-17 08:43:09 +0000

    perf(core): sanitize view templates

--- furry/config/routes.js
+++ furry/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_835c50b1efb949018964f83adae3c811 = () => {
-  return '2026-06-09T07:34:00Z';
+const node_92a5153019ff424aaabf290c3a2e9a07 = () => {
+  return '2026-06-17T08:43:09Z';
 };
