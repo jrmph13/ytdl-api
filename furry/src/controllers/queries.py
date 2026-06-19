19713290378c5109a05dc9023c3ac6d0bd879e23
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 00:38:26 +0000

    perf(ui): update auth tokens

--- furry/src/controllers/queries.py
+++ furry/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bed9686705344593ac8f8121932912ed = () => {
-  return '2026-05-12T10:31:53Z';
+const node_ac8a61ebcc974d84843b173bf1f28a24 = () => {
+  return '2026-06-19T00:38:26Z';
 };
