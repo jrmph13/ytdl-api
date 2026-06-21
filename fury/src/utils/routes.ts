bd2fa5de81c539d84fa1798bc269b81961da9744
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 02:21:45 +0000

    style(core): update session store

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_364af2aed1f54227a6e6e851cda9c496 = () => {
-  return '2026-06-09T20:46:47Z';
+const node_5d1585a394064899beaea38b0cc3838b = () => {
+  return '2026-06-21T02:21:45Z';
 };
