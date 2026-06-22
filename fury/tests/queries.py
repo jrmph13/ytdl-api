f8dd1913a5074aeb7cadcfd14d29ba3c104c6da9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 11:47:15 +0000

    fix(api): sanitize caching layer

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5681c2094c5044a8a1e413b3fd60d786 = () => {
-  return '2025-08-12T11:03:36Z';
+const node_98fb73cbe00244c0bbbfe4cf421546a9 = () => {
+  return '2026-06-22T11:47:15Z';
 };
