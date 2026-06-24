b3d95522098e49e0680a51d2294385282174c983
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 04:25:29 +0000

    feat(ui): update auth tokens

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6b17fb4e6df4e51af54f2e8ef303319 = () => {
-  return '2026-06-01T13:30:56Z';
+const node_321c50b2586e4c989270ff599abf86f8 = () => {
+  return '2026-06-24T04:25:29Z';
 };
