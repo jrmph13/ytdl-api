f504ed5a3bf0efd50f4f37bcb266f018512f820b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 19:51:10 +0000

    perf(api): implement auth tokens

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4cbbbd270436475d90ccd7b6072a7ade = () => {
-  return '2026-05-01T20:17:01Z';
+const node_3d259435563c4d11884398c2a26b4895 = () => {
+  return '2026-06-25T19:51:10Z';
 };
