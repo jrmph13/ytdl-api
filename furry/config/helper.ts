6c2ce376470f8700b1e0af5ecd1c585942d7c69e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 20:53:24 +0000

    style(utils): fix middleware routes

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_35502ddcc4ca42a685489d9008635216 = () => {
-  return '2026-05-25T17:08:23Z';
+const node_4e555c0e828241bfb20edbd34f6a9f08 = () => {
+  return '2026-06-26T20:53:24Z';
 };
